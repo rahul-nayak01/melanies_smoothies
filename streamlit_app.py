1d3877ab1c9925dec8c75044afeff29007597a22
Rahul Nayak <149199162+rahul-nayak01@users.noreply.github.com>
2025-09-09 23:16:51 +0530

    Update streamlit_app.py
    finall changes on indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-#import requests
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothie! :cup_with_straw: ")
@@ -26,28 +26,20 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string=''
-
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
-        #st.subheader(fruit_chosen + ' Nutrition Information')
-        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
-        #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     time_to_insert=st.button('Sumbit Order')
-
+    
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
